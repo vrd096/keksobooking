0de4b7f7c9cc42d5d225cabf768446d409d8c11d
Evgeniy <vrd096@mail.ru>
2019-06-26 16:28:54 +0500

    перенес в map.js взаимодествие с главным пином

--- js/map.js
+++ js/map.js
@@ -13,4 +13,29 @@
 
     document.querySelector(".map").appendChild(fragmentPopup);
   })();
+
+  var pinMain = document.querySelector(".map__pin--main");
+  var noticeForm = document.querySelector(".notice__form");
+  var noticeAddress = noticeForm.querySelector("#address");
+  var mapForm = document.querySelectorAll(".map__filter");
+  var pinElement = document.querySelectorAll(".map__pin");
+
+  var axesPin = function() {
+    var pinX = Math.round(pinMain.offsetLeft);
+    var pinY = Math.round(pinMain.offsetTop + 50);
+    return (noticeAddress.value = pinX + "," + pinY);
+  };
+  axesPin();
+
+  pinMain.addEventListener("mouseup", function() {
+    for (var i = 0; i < pinElement.length; i++) {
+      pinElement[i].classList.remove("hidden");
+    }
+    for (var i = 0; i < mapForm.length; i++) {
+      mapForm[i].removeAttribute("disabled");
+    }
+    map.classList.remove("map--faded");
+    noticeForm.classList.remove("notice__form--disabled");
+    axesPin();
+  });
 })();
