94566a23f344ad4610b00fad3e33a82035e2f87b
Evgeniy <vrd096@mail.ru>
2019-07-04 14:15:07 +0500

    создал backend и создал функцию загрузки

---
+++ js/backend.js
@@ -0,0 +1,62 @@
+"use strict";
+/*
+создать глобальные функции
+получать данные с помощью XMLHttpRequest
+обрабатывать данные и передавать через коллбек
+отправлять данные на сервер
+
+Функция получения данных:
+onLoad - callback при успешном выполнение запроса, в параметр набор полученных данных
+onError - callback при неуспешном выполнении запроса, в параметр попадает сообщение о ошибке
+отрисовку объектов брать с сервера , а не из рандомной отрисовки
+
+Функция отправки данных:
+data - объект FormData
+onLoad - callback при успешном выполнении запроса
+onError - callback при неуспешном выполнении запроса, при вызове сообщение о ошибке или объект с описанием ошибки полученный с сервера
+
+
+*/
+
+(function() {
+  window.load = function(onLoad, onError) {
+    var URL = "https://js.dump.academy/keksobooking/data";
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+
+    xhr.addEventListener("load", function() {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError("Статус ошибки: " + xhr.status);
+      }
+    });
+
+    xhr.addEventListener("error", function() {
+      onError("Произошла ошибка соеденения");
+    });
+
+    xhr.addEventListener("timeout", function() {
+      onError("Запрос не успел выполниться за " + xhr.timeout + "мс");
+    });
+
+    xhr.timeout = 5000;
+
+    xhr.open("GET", URL);
+    this.console.log(xhr);
+    xhr.send();
+  };
+})();
+
+var successLoad = function(pins) {
+  for (var currentPin = 0; currentPin < pins.length; currentPin++) {
+    console.log(pins[currentPin]);
+  }
+};
+var errorLoad = function(error) {
+  
+    console.log(error);
+ 
+};
+
+window.load(successLoad, errorLoad);
