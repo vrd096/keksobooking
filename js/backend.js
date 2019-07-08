df103b591f2bdc922b20d5863cceb28c7293b110
Evgeniy <vrd096@mail.ru>
2019-07-08 12:37:35 +0500

    сделал фукнцию загрузки формы

--- js/backend.js
+++ js/backend.js
@@ -46,6 +46,34 @@ onError - callback при неуспешном выполнении запрос
 
     xhr.send();
   };
+
+  window.saveForm = function(data, onLoad, onError) {
+    var URL = "https://js.dump.academy/keksobooking";
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+
+    xhr.addEventListener("load", function() {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError("Статус ошибки: " + xhr.status);
+      }
+    });
+
+    xhr.addEventListener("error", function() {
+      onError("Произошла ошибка соеденения");
+    });
+
+    xhr.addEventListener("timeout", function() {
+      onError("Запрос не успел выполниться за " + xhr.timeout + "мс");
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open("POST", URL);
+
+    xhr.send(data);
+  };
 })();
 
 // var successLoad = function(pins) {
--- js/form.js
+++ js/form.js
@@ -3,6 +3,7 @@
 (function() {
   var numberRoom = document.querySelector("#room_number");
   var numberPeople = document.querySelector("#capacity");
+  var formUpload = document.querySelector(".notice__form");
 
   numberPeople.addEventListener("change", function() {
     for (var i = 0; i < numberRoom.value; i++) {
@@ -12,5 +13,18 @@
         numberPeople.setCustomValidity("");
       }
     }
+  });
+
+  function resetForm() {
+    formUpload.reset();
+  }
+
+  function errorHandler(error) {
+    console.log(error);
+  }
+
+  formUpload.addEventListener("submit", function(evt) {
+    window.saveForm(new FormData(formUpload), resetForm, errorHandler);
+    evt.preventDefault();
   });
 })();
